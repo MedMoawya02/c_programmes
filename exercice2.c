197dde4569ca5f149aba7fb23da15cce2d2b12e6
MedMoawya02 <mohammed.mouawya2002@gmail.com>
2025-09-02 11:54:44 +0200

    test

---
+++ exercice2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    float v,r;
+    const float Pi=3.14;
+    r=10;
+    v=4.0/3.0*Pi*pow(r,3);
+    printf("levolume de sphere = %f",v);
+return 0;
+}
